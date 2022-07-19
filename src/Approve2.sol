b1e02057df4372208230487744c48788322644a5
t11s <greenfeatherhelp@gmail.com>
2022-07-19 14:47:15 -0700

    🚧 WIP

--- src/Approve2.sol
+++ src/Approve2.sol
@@ -4,58 +4,60 @@ pragma solidity 0.8.13;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
+// todo: could use create2 based system for some interesting properties
+
 contract Approve2 {
     using SafeTransferLib for ERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                            DANGERLIST LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    uint128 constant PARADIGM_DANGERLIST = 0;
+
+    mapping(uint128 => address) dangerListOwners;
+
+    mapping(uint128 => mapping(address => bool)) dangerList;
 
     /*//////////////////////////////////////////////////////////////
                              EIP-712 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    uint256 internal immutable INITIAL_CHAIN_ID;
+    struct UserData {
+        uint128 nonce;
+        uint128 dangerListId;
+    }
 
-    bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
+    // todo: raw bitpacking would be cheaper
 
-    mapping(address => uint256) public nonces;
+    mapping(address => UserData) public getUserData;
+
+    mapping(address => mapping(address => bool)) hasApprovedAll;
 
     /*//////////////////////////////////////////////////////////////
                             ALLOWANCE STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(ERC20 => mapping(address => mapping(address => uint256))) public allowance;
 
-    event Permit(address indexed owner, address indexed spender, uint256 amount);
-
-    constructor() {
-        INITIAL_CHAIN_ID = block.chainid;
-        INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
-    }
+    event Permit(ERC20 indexed token, address indexed owner, address indexed spender, uint256 amount);
 
     /*//////////////////////////////////////////////////////////////
-                              EIP-712 LOGIC
+                            APPROVE ALL LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
-        return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
+    function setApprovedAll(address to, bool approvedAll) public {
+        hasApprovedAll[msg.sender][to] = approvedAll;
     }
 
-    function computeDomainSeparator() internal view virtual returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                    keccak256("Para Permit"),
-                    keccak256("1"),
-                    block.chainid,
-                    address(this)
-                )
-            );
-    }
+    // TODO: need a permit for this
 
     /*//////////////////////////////////////////////////////////////
                               PERMIT LOGIC
     //////////////////////////////////////////////////////////////*/
 
     function permit(
+        ERC20 token,
         address owner,
         address spender,
         uint256 value,
@@ -63,7 +65,7 @@ contract Approve2 {
         uint8 v,
         bytes32 r,
         bytes32 s
-    ) public virtual {
+    ) external virtual {
         require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
 
         // Unchecked because the only math done is incrementing
@@ -73,14 +75,22 @@ contract Approve2 {
                 keccak256(
                     abi.encodePacked(
                         "\x19\x01",
-                        DOMAIN_SEPARATOR(),
+                        keccak256(
+                            abi.encode(
+                                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                                keccak256("Approve2"), // We use unique a unique name and version to ensure if
+                                keccak256("Approve2 v1"), // the token ever adds permit support we don't collide.
+                                block.chainid,
+                                address(token) // We use the token's address for easy frontend compatibility.
+                            )
+                        ),
                         keccak256(
                             abi.encode(
                                 keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
                                 owner,
                                 spender,
                                 value,
-                                nonces[owner]++,
+                                ++getUserData[owner].nonce,
                                 deadline
                             )
                         )
@@ -93,10 +103,10 @@ contract Approve2 {
 
             require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_SIGNER");
 
-            allowance[recoveredAddress][spender] = value;
+            allowance[token][recoveredAddress][spender] = value;
         }
 
-        emit Permit(owner, spender, value);
+        emit Permit(token, owner, spender, value);
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -108,13 +118,43 @@ contract Approve2 {
         address from,
         address to,
         uint256 amount
-    ) public virtual returns (bool) {
-        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+    ) external virtual returns (bool) {
+        uint256 allowed = allowance[token][from][msg.sender]; // Saves gas for limited approvals.
 
-        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+        if (allowed != type(uint256).max) allowance[token][from][msg.sender] = allowed - amount;
 
         token.safeTransferFrom(from, to, amount);
 
+        // TODO: event?
+
         return true;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             USER DATA LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function invalidateNonce() external {
+        ++getUserData[msg.sender].nonce;
+    }
+
+    function chooseDangerList(uint128 dangerListId) external {
+        getUserData[msg.sender].dangerListId = dangerListId;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            DANGERLIST LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function claimDangerlist(uint128 dangerListId) external {
+        require(dangerListOwners[dangerListId] == address(0), "DANGERLIST_ALREADY_CLAIMED");
+
+        dangerListOwners[dangerListId] = msg.sender;
+    }
+
+    function setDangerlistStatus(uint128 dangerListId, bool status) external {
+        require(dangerListOwners[dangerListId] == msg.sender, "NOT_DANGERLIST_OWNER");
+
+        dangerList[dangerListId][msg.sender] = status;
     }
 }
--- src/Approve2Lib.sol
+++ src/Approve2Lib.sol
@@ -42,12 +42,48 @@ library Approve2Lib {
         address to,
         uint256 amount
     ) internal {
+
+        if (safeTransferFrom)
+
         if (token.allowance(from, address(this)) >= amount) {
             // Use normal transfer if possible.
             token.safeTransferFrom(from, to, amount);
         } else {
             // Otherwise try Approve2 (assume permit has already happened).
             permit.transferFrom(token, from, to, amount);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           SAFE TRANSFER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function safeTransferFrom(
+        ERC20 token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal returns (bool success) {
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata into memory, beginning with the function selector.
+            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000)
+            mstore(add(freeMemoryPointer, 4), from) // Append the "from" argument.
+            mstore(add(freeMemoryPointer, 36), to) // Append the "to" argument.
+            mstore(add(freeMemoryPointer, 68), amount) // Append the "amount" argument.
+
+            success := and(
+                // Set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data.
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // We use 100 because the length of our calldata totals up like so: 4 + 32 * 3.
+                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+                // Counterintuitively, this call must be positioned second to the or() call in the
+                // surrounding and() call or else returndatasize() will be zero during the computation.
+                call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
+            )
         }
     }
 }
