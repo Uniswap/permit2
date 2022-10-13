94b5fe0643604537c23e09c7bc458d5c64ffdd64
Sara Reynolds <snreynolds2506@gmail.com>
2022-10-13 10:53:32 -0400

    tests and utils

---
+++ test/mocks/MockERC721.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
+contract MockERC721 is ERC721 {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mint(address to, uint256 id) public {
+        _mint(to, id);
+    }
+
+    function tokenURI(uint256) public view virtual override returns (string memory) {
+        return "";
+    }
+}
