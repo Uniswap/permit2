19a75ff86ff3e844f2d7c8ce492210cf37255287
marktoda <toda.mark@gmail.com>
2022-10-17 15:41:49 -0700

    feat: add cached domain separator (#1)
    This commit adds a caching domain separator which allows the contract to
    continue to function after a fork or other event that changes the
    chainid or contract address after construction

---
+++ src/base/DomainSeparator.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+/// @notice caching domain separator
+/// @dev maintains cross-chain replay protection in the event of a fork
+/// @dev reference: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/EIP712.sol
+contract DomainSeparator {
+    /* solhint-disable var-name-mixedcase */
+    // Cache the domain separator as an immutable value, but also store the chain id that it corresponds to, in order to
+    // invalidate the cached domain separator if the chain id changes.
+    bytes32 private immutable _CACHED_DOMAIN_SEPARATOR;
+    uint256 private immutable _CACHED_CHAIN_ID;
+    address private immutable _CACHED_THIS;
+
+    bytes32 private constant _HASHED_NAME = keccak256("Permit2");
+    bytes32 private constant _HASHED_VERSION = keccak256("1");
+    bytes32 private constant _TYPE_HASH =
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+
+    constructor() {
+        _CACHED_CHAIN_ID = block.chainid;
+        _CACHED_THIS = address(this);
+        _CACHED_DOMAIN_SEPARATOR = _buildDomainSeparator(_TYPE_HASH, _HASHED_NAME, _HASHED_VERSION);
+    }
+
+    /// @notice returns the domain separator for the current chain
+    /// @dev uses cached version if chainid and address are unchanged from construction
+    function _domainSeparatorV4() internal view returns (bytes32) {
+        if (address(this) == _CACHED_THIS && block.chainid == _CACHED_CHAIN_ID) {
+            return _CACHED_DOMAIN_SEPARATOR;
+        } else {
+            return _buildDomainSeparator(_TYPE_HASH, _HASHED_NAME, _HASHED_VERSION);
+        }
+    }
+
+    /// @notice builds a domain separator using the current chainId and contract address
+    function _buildDomainSeparator(bytes32 typeHash, bytes32 nameHash, bytes32 versionHash)
+        private
+        view
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(typeHash, nameHash, versionHash, block.chainid, address(this)));
+    }
+}
