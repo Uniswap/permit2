fd55ec714a901309e7874174db42bb543593bc3e
Noah Zinsmeister <noahwz@gmail.com>
2022-10-17 14:36:23 -0400

    solidity 0.8.17

--- src/Approve2Lib.sol
+++ src/Approve2Lib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.17;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Bytes32AddressLib} from "solmate/utils/Bytes32AddressLib.sol";
