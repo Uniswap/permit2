c258b8941cb5671cb515b772d4f59f0b345375ef
Noah Zinsmeister <noahwz@gmail.com>
2022-10-17 17:32:19 -0400

    make license ids consistent

--- test/mocks/MockERC1155.sol
+++ test/mocks/MockERC1155.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import {ERC1155} from "solmate/tokens/ERC1155.sol";
--- test/mocks/MockERC721.sol
+++ test/mocks/MockERC721.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
