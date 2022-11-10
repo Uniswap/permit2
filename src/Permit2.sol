ae10dbc1748e8f09c72526cee937fd19a6134a3d
Sara Reynolds <snreynolds2506@gmail.com>
2022-11-10 18:41:33 -0500

    add comment in empty block

--- src/Permit2.sol
+++ src/Permit2.sol
@@ -4,4 +4,7 @@ pragma solidity 0.8.17;
 import {SignatureTransfer} from "./SignatureTransfer.sol";
 import {AllowanceTransfer} from "./AllowanceTransfer.sol";
 
-contract Permit2 is SignatureTransfer, AllowanceTransfer {}
+contract Permit2 is SignatureTransfer, AllowanceTransfer {
+// Permit2 handles signature-based transfers in SignatureTransfer and allowance-based transfers in AllowanceTransfer.
+// Permit2 unifies them so users have maximal flexibility with their approval.
+}
