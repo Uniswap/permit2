ef81fe18e0d85b05555afea5aa0954608a33d9e5
Sara Reynolds <snreynolds2506@gmail.com>
2022-11-15 17:12:32 -0500

    permit single

--- test/TypehashGeneration.t.sol
+++ test/TypehashGeneration.t.sol
@@ -32,10 +32,10 @@ contract TypehashGeneration is Test, PermitSignature, TokenProvider {
     MockSignatureVerification mockSig;
 
     function setUp() public {
+        vm.chainId(1);
         permit2 = new MockPermit2();
 
         verifyingContract = address(permit2);
-        console2.log(verifyingContract);
 
         token = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
         spender = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
@@ -44,23 +44,17 @@ contract TypehashGeneration is Test, PermitSignature, TokenProvider {
         sigDeadline = 146902158100;
         nonce = 0;
 
-        console2.log(amount);
-
         DOMAIN_SEPARATOR = permit2.DOMAIN_SEPARATOR();
 
         mockSig = new MockSignatureVerification();
     }
 
-    function testPermitSingle() public {
+    function testPermitSingle() public view {
         // metamask wallet signed data
         // 0xdb5507adaba8ed8e1d83dc7cb64980735c4769076c657d80563ce9a991fbb1981d07973917923c7942307e63285ff2e9e8d435fc4da8cdc7546a669bf474fb6d1b
         bytes32 r = 0xdb5507adaba8ed8e1d83dc7cb64980735c4769076c657d80563ce9a991fbb198;
         bytes32 s = 0x1d07973917923c7942307e63285ff2e9e8d435fc4da8cdc7546a669bf474fb6d;
         uint8 v = 0x1b;
-
-        console2.logBytes32(r);
-        console2.logBytes32(s);
-        console2.log(v);
 
         bytes memory sig = bytes.concat(r, s, bytes1(v));
 
@@ -72,13 +66,7 @@ contract TypehashGeneration is Test, PermitSignature, TokenProvider {
             IAllowanceTransfer.PermitSingle({details: details, spender: spender, sigDeadline: sigDeadline});
 
         // generate hash of local data
-        bytes32 hashedPermit = permit.hash();
-
-        // extra check: generate sig of local data and check that it equals the metamask sig
-        (uint8 v1, bytes32 r1, bytes32 s1) = getPermitSignatureRaw(permit, PRIV_KEY_TEST, DOMAIN_SEPARATOR);
-        assertEq(v, v1);
-        assertEq(r, r1);
-        assertEq(s, s1);
+        bytes32 hashedPermit = keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, permit.hash()));
 
         // verify the signed data againt the locally generated hash
         // this should not revert, validating that from is indeed the signer
