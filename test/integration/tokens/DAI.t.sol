b555d84332e61e9ffb3265d0d64a333e53ed8e27
Mark Toda <toda.mark@gmail.com>
2022-11-15 18:29:54 -0800

    feat: add fork token tests
    This commit adds some mainnet fork tests using permit2 against known
    existing tokens. Currently using USDC, USDC, DAI and UNI

---
+++ test/integration/tokens/DAI.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MainnetTokenTest} from "../MainnetToken.t.sol";
+
+contract DAITest is MainnetTokenTest {
+    function token() internal pure override returns (ERC20) {
+        return ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    }
+}
---
+++ test/integration/tokens/USDC.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MainnetTokenTest} from "../MainnetToken.t.sol";
+
+contract USDCTest is MainnetTokenTest {
+    function token() internal pure override returns (ERC20) {
+        return ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    }
+}
