1bc9a7cd91190bd9476af30741bd74b4f7cad31d
marktoda <toda.mark@gmail.com>
2022-11-16 17:16:27 -0800

    feat: update salt (#148)
    New Permit2 address: 0x000000000022D473030F116dDEE9F6B43aC78BA3

--- script/DeployPermit2.s.sol
+++ script/DeployPermit2.s.sol
@@ -5,7 +5,7 @@ import "forge-std/console2.sol";
 import "forge-std/Script.sol";
 import {Permit2} from "src/Permit2.sol";
 
-bytes32 constant SALT = bytes32(uint256(0x0000000000000000000000000000000000000000ef81e836c9a70801514b96e9));
+bytes32 constant SALT = bytes32(uint256(0x0000000000000000000000000000000000000000d3af2663da51c10215000000));
 
 contract DeployPermit2 is Script {
     function setUp() public {}
