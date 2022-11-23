4a96b2ea760a91ae3d9a698066b60b8f2e5e4efd
codeislight1 <codeislight@gmail.com>
2022-11-23 08:35:52 +0800

    optimize safeCast160 revert

--- src/libraries/SafeCast160.sol
+++ src/libraries/SafeCast160.sol
@@ -2,13 +2,20 @@
 pragma solidity ^0.8.17;
 
 library SafeCast160 {
-    /// @notice Thrown when a valude greater than type(uint160).max is cast to uint160
+    /// @notice Thrown when a value greater than type(uint160).max is cast to uint160
     error UnsafeCast();
 
     /// @notice Safely casts uint256 to uint160
     /// @param value The uint256 to be cast
-    function toUint160(uint256 value) internal pure returns (uint160) {
-        if (value > type(uint160).max) revert UnsafeCast();
-        return uint160(value);
+    function toUint160(uint256 value) internal pure returns (uint160 result) {
+        uint256 maxValue = type(uint160).max;
+        assembly {
+            if gt(value, maxValue) {
+                let ptr := mload(0x40)
+                mstore(ptr, 0xc4bd89a9)
+                revert(add(ptr, 0x1C), 0x04)
+            }
+            result := value
+        }
     }
 }
