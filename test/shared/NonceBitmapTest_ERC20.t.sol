1ea812cac45bb11628a739c9f59e8ed1b7f1df5e
Sara Reynolds <snreynolds2506@gmail.com>
2023-01-06 17:42:15 -0500

    721 bitmap tests

--- test/shared/NonceBitmapTest_ERC20.t.sol
+++ test/shared/NonceBitmapTest_ERC20.t.sol
@@ -9,4 +9,12 @@ contract NonceBitmapTest_ERC20 is BaseNonceBitmapTest {
     function setUp() public override {
         permit2 = new MockPermit2();
     }
+
+    function invalidateUnorderedNonces(uint256 wordPos, uint256 mask) public override {
+        MockPermit2(address(permit2)).invalidateUnorderedNonces(wordPos, mask);
+    }
+
+    function nonceBitmap(address addr, uint256 wordPos) public override returns (uint256) {
+        return MockPermit2(address(permit2)).nonceBitmap(addr, wordPos);
+    }
 }
